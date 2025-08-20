8ca1bdbc8d12318c75ad1cd02530e8e056f07d53
IbrahimPatel-63 <ibrahimpatel6378@gmail.com>
2025-08-20 21:27:39 +0530

    Fix some code

--- src/Components/Upperline.jsx
+++ src/Components/Upperline.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Upperline() {
   return (
     <div className='h-10 w-full md:h-11  bg-[#D8CEDF] flex items-center justify-center font-[Geologica]'>
-        <p className='text-[16px] tracking-wider md:text-[16px] '>Free international shipping for orders over <span className='text-[#E24E22]'>$400</span></p>
+        <p className='text-[14px] tracking-wider md:text-[16px] '>Free international shipping for orders over <span className='text-[#E24E22]'>$400</span></p>
       
     </div>
   )
